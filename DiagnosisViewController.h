54a58a3e78332166f5d3bc83dee6d909dc6c9cf0
Waqas <waqasdurrani29@gmail.com>
2019-05-08 07:38:45 +0200

    Update DiagnosisViewController.h

--- DiagnosisViewController.h
+++ DiagnosisViewController.h
@@ -1,10 +1,4 @@
-//
-//  DiagnosisViewController.h
-//  ThePhenomizer
-//
-//  Created by SE15 UniB on 06/01/16.
-//  Copyright © 2016 SE15 UniB. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
