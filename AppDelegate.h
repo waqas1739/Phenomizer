ffe50bab93a4a1a99ead7a5574b05e91828dbb91
Waqas <waqasdurrani29@gmail.com>
2019-05-08 07:31:59 +0200

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThePhenomizer
+//
+//  Created by SE15 UniB on 01/12/15.
+//  Copyright © 2015 SE15 UniB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
