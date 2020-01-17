a586eaedbcbd55994a3f415a7ab657dd81c6f9b4
Waqas <waqasdurrani29@gmail.com>
2020-01-17 11:37:23 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,3 @@
-//
-//  main.m
-//  ThePhenomizer
-//
-//  Created by SE15 UniB on 01/12/15.
-//  Copyright © 2015 SE15 UniB. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
